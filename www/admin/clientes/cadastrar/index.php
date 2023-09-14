0280dd217e46a7d1096991896abfcddd7d205fc7
Gabriel R. Antunes <gabrielrodantunes@gmail.com>
2023-09-14 15:28:45 -0400

    feat: cadastrar endereço do cliente

--- www/admin/clientes/cadastrar/index.php
+++ www/admin/clientes/cadastrar/index.php
@@ -1,88 +1,77 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../../../css/bootstrap.min.css" />
-    <link rel="stylesheet" href="../../../css/geral.css" />
 
-    <title>Cadastro Cliente</title>
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="../../../css/bootstrap.min.css" />
+  <link rel="stylesheet" href="../../../css/geral.css" />
 
-    <script type="module" src="../../../js/pw-web-components/pw-web-components.esm.js"></script>
-    <script nomodule src="../../../js/pw-web-components/pw-web-components.js"></script>
-  </head>
-  <body data-pw-config-base-url="../../../">
-    <pw-header active-link="painel"></pw-header>
+  <title>Cadastro Cliente</title>
 
-    <pw-painel-layout active-link="clientes-cadastrar">
-      <div class="container px-4">
-        <h1 class="titulo my-3">Cadastrar Cliente</h1>
+  <script type="module" src="../../../js/pw-web-components/pw-web-components.esm.js"></script>
+  <script nomodule src="../../../js/pw-web-components/pw-web-components.js"></script>
+</head>
 
-        <form action="script.php" method="post" id="cadastroForm" class="needs-validation" novalidate>
-          <div class="mb-3">
-            <label for="nome">Nome Completo:</label>
-            <input type="text" id="nome" class="form-control" placeholder="Nome Completo" name="nome" required />
-            <div class="invalid-feedback">Por favor, preencha o nome completo.</div>
+<body data-pw-config-base-url="../../../">
+  <pw-header active-link="painel"></pw-header>
+
+  <pw-painel-layout active-link="clientes-cadastrar">
+    <div class="container px-4">
+      <h1 class="titulo my-3">Cadastrar Cliente</h1>
+
+      <form action="script.php" method="post" id="cadastroForm" class="needs-validation" novalidate>
+        <div class="mb-3">
+          <label for="nome">Nome Completo:</label>
+          <input type="text" id="nome" class="form-control" placeholder="Nome Completo" name="nome" required />
+          <div class="invalid-feedback">Por favor, preencha o nome completo.</div>
+        </div>
+
+        <div class="mb-3">
+          <label for="dataNascimentoInput" class="form-label">Data de Nascimento:</label>
+          <input type="text" class="form-control" id="dataNascimentoInput" placeholder="DD/MM/AAAA"
+            name="dataNascimento" required />
+          <div class="invalid-feedback">Por favor, insira uma data de nascimento válida.</div>
+        </div>
+
+        <div class="mb-3">
+          <label for="email">Email:</label>
+          <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Email" aria-label="email"
+              aria-describedby="basic-addon2" name="email" required
+              pattern="^[\w.-]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,})+$" />
+            <div class="invalid-feedback">Por favor, insira um email válido.</div>
+          </div>
+        </div>
+
+        <div class="mb-3">
+          <label for="telefone">Telefone:</label>
+          <div class="input-group mb-3">
+            <span class="input-group-text">+55</span>
+            <input type="tel" id="telefone" name="telefone" class="form-control" placeholder="Telefone" required
+              pattern="[0-9]+" />
+            <div class="invalid-feedback">Por favor, insira um telefone válido.</div>
+          </div>
+        </div>
+
+        <hr>
+
+        <div class="row g-3">
+          <div class="col-md-3">
+            <label for="enderecoCep" class="form-label">CEP</label>
+
+            <input type="text" class="form-control" id="enderecoCep" name="enderecoCep" required />
+
+            <div class="invalid-feedback">Por favor, insira um CEP.</div>
           </div>
 
-          <div class="mb-3">
-            <label for="dataNascimentoInput" class="form-label">Data de Nascimento:</label>
-            <input
-              type="text"
-              class="form-control"
-              id="dataNascimentoInput"
-              placeholder="DD/MM/AAAA"
-              name="dataNascimento"
-              required
-            />
-            <div class="invalid-feedback">Por favor, insira uma data de nascimento válida.</div>
-          </div>
 
-          <div class="mb-3">
-            <label for="email">Email:</label>
-            <div class="input-group mb-3">
-              <input
-                type="text"
-                class="form-control"
-                placeholder="Email"
-                aria-label="email"
-                aria-describedby="basic-addon2"
-                name="email"
-                required
-                pattern="^[\w.-]+@[a-zA-Z_-]+?(?:\.[a-zA-Z]{2,})+$"
-              />
-              <div class="invalid-feedback">Por favor, insira um email válido.</div>
-            </div>
-          </div>
+          <div class="col-md-3">
+            <label for="enderecoUf" class="form-label">UF</label>
 
-          <div class="mb-3">
-            <label for="telefone">Telefone:</label>
-            <div class="input-group mb-3">
-              <span class="input-group-text">+55</span>
-              <input
-                type="tel"
-                id="telefone"
-                name="telefone"
-                class="form-control"
-                placeholder="Telefone"
-                required
-                pattern="[0-9]+"
-              />
-              <div class="invalid-feedback">Por favor, insira um telefone válido.</div>
-            </div>
-          </div>
-
-          <div class="mb-3">
-            <label for="cidade">Cidade:</label>
-            <input type="text" id="cidade" name="cidade" class="form-control" placeholder="Cidade" required />
-            <div class="invalid-feedback">Por favor, preencha a cidade.</div>
-          </div>
-
-          <div class="mb-3">
-            <label for="estado">Estado:</label>
-            <select name="estado" id="estado" class="form-select" required>
-              <option value="">Selecione um estado</option>
+            <select class="form-select" id="enderecoUf" name="enderecoUf" required>
+              <option selected disabled value="">Selecione...</option>
               <option value="AC">Acre</option>
               <option value="AL">Alagoas</option>
               <option value="AP">Amapá</option>
@@ -110,33 +99,63 @@
               <option value="SP">São Paulo</option>
               <option value="SE">Sergipe</option>
               <option value="TO">Tocantins</option>
+              <option value="EX">Estrangeiro</option>
             </select>
-            <div class="invalid-feedback">Por favor, selecione um estado.</div>
+            <div class="invalid-feedback">Por favor, selecione um estado!</div>
           </div>
 
-          <div class="mb-3">
-            <label for="endereco">Endereço:</label>
-            <input type="text" id="endereco" name="endereco" class="form-control" placeholder="Endereço" required />
-            <div class="invalid-feedback">Por favor, preencha o endereço.</div>
+          <div class="col-md-6">
+            <label for="enderecoCidade" class="form-label">Cidade</label>
+
+            <input type="text" class="form-control" id="enderecoCidade" name="enderecoCidade" required />
+
+            <div class="invalid-feedback">Por favor, insira uma cidade.</div>
           </div>
 
-          <div class="mb-3">
-            <label for="genero">Gênero:</label>
-            <select name="genero" id="genero" class="form-select" required>
-              <option value="">Selecione um gênero</option>
-              <option value="Masculino">Masculino</option>
-              <option value="Feminino">Feminino</option>
-              <option value="Outro">Outro</option>
-            </select>
-            <div class="invalid-feedback">Por favor, selecione um gênero.</div>
+          <div class="col-md-6">
+            <label for="enderecoRua" class="form-label">Rua</label>
+            <input type="text" class="form-control" id="enderecoRua" name="enderecoRua" required />
+            <div class="invalid-feedback">Por favor, insira uma rua.</div>
           </div>
 
-          <button type="submit" class="btn btn-primary">Cadastrar</button>
-        </form>
-      </div>
-    </pw-painel-layout>
+          <div class="col-md-2">
+            <label for="enderecoNumero" class="form-label">Número</label>
 
-    <script src="../../../js/validacliente.js"></script>
-    <script src="../../../js/bootstrap.min.js"></script>
-  </body>
+            <input type="text" class="form-control" id="enderecoNumero" name="enderecoNumero" required />
+
+            <div class="invalid-feedback">Por favor, insira um número.</div>
+          </div>
+
+          <div class="col-md-4">
+            <label for="enderecoBairro" class="form-label">Bairro</label>
+
+            <input type="text" class="form-control" id="enderecoBairro" name="enderecoBairro" required />
+
+            <div class="invalid-feedback">Por favor, insira um bairro.</div>
+          </div>
+        </div>
+        <hr>
+
+        <div class="mb-3">
+          <label for="genero">Gênero:</label>
+
+          <select name="genero" id="genero" class="form-select" required>
+            <option value="">Selecione um gênero</option>
+            <option value="Masculino">Masculino</option>
+            <option value="Feminino">Feminino</option>
+            <option value="Outro">Outro</option>
+          </select>
+
+          <div class="invalid-feedback">Por favor, selecione um gênero.</div>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Cadastrar</button>
+      </form>
+    </div>
+  </pw-painel-layout>
+
+  <script src="../../../js/validacliente.js"></script>
+  <script src="../../../js/bootstrap.min.js"></script>
+</body>
+
 </html>
--- www/admin/clientes/cadastrar/script.php
+++ www/admin/clientes/cadastrar/script.php
@@ -2,61 +2,80 @@
 
 require_once("../../../../php/_conexao.php");
 
-if($_SERVER["REQUEST_METHOD"] === "POST") {
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
   $nome = $_POST["nome"];
   $dataNascimento = $_POST["dataNascimento"];
   $email = $_POST["email"];
   $telefone = $_POST["telefone"];
-  $cidade = $_POST["cidade"];
-  $estado = $_POST["estado"];
-  $endereco = $_POST["endereco"];
   $genero = $_POST["genero"];
+
+
+  $enderecoPais = "Brasil";
+  $enderecoCep = $_POST["enderecoCep"];
+  $enderecoUf = $_POST["enderecoUf"];
+  $enderecoCidade = $_POST["enderecoCidade"];
+  $enderecoRua = $_POST["enderecoRua"];
+  $enderecoNumero = $_POST["enderecoNumero"];
+  $enderecoBairro = $_POST["enderecoBairro"];
+  $enderecoComplemento = "";
 
   $_con = bd_conectar();
 
-  // prepare and bind
-  $stmt = $_con->prepare("INSERT INTO `cliente` 
+  $stmtEndereco = $_con->prepare("INSERT INTO `endereco` (`id_end`, `pais_end`, `codigo_postal_end`, `uf_end`, `cidade_end`, `rua_end`, `numero_end`, `bairro_end`, `complemento_end`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?)");
+
+  $stmtEndereco->bind_param("sssssiss", $enderecoPais, $enderecoCep, $enderecoUf, $enderecoCidade, $enderecoRua, $enderecoNumero, $enderecoBairro, $enderecoComplemento);
+
+  $stmtEndereco->execute();
+  $enderecoId = $stmtEndereco->insert_id;
+
+  $stmtCliente = $_con->prepare("INSERT INTO `cliente` 
     (`id_cli`, `nome_cli`, `telefone_cli`, `email_cli`, `genero_cli`, `id_end_fk`) 
-    VALUES (NULL, ?, ?, ?, ?, NULL)
+    VALUES (NULL, ?, ?, ?, ?, ?)
   ");
 
-  $stmt->bind_param("ssss", $nome, $telefone, $email, $genero);
+  $stmtCliente->bind_param("ssssi", $nome, $telefone, $email, $genero, $enderecoId);
 
-  $stmt->execute();
+  $stmtCliente->execute();
 
-  if($stmt->affected_rows === 1) { ?>
-  
+  if ($stmtCliente->affected_rows === 1) { ?>
+
     <!DOCTYPE html>
     <html lang="pt-br">
+
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
+
     <body>
       <p>Cliente cadastrado com sucesso!</p>
       <a href=".">Voltar</a>
     </body>
+
     </html>
 
-  <?php
+    <?php
   } else { ?>
-  
+
     <!DOCTYPE html>
     <html lang="pt-br">
+
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
+
     <body>
       <p>Não foi possível cadastrar o cliente.</p>
       <a href=".">Voltar</a>
     </body>
+
     </html>
 
-  <?php
+    <?php
   }
 
-  $stmt->close();
+  $stmtCliente->close();
 
   bd_desconectar($_con);
 }
